e0df17c472aebf6923c1289dcb6e86a83577fba8
Ribemont Francois <ribemont.francois@gmail.com>
2013-09-16 14:00:19 +0000

    New default values added

--- system-services/usr/local/lib/values.py
+++ system-services/usr/local/lib/values.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+""" Contains the default values for the AF_SYN_RT program """
+
 import logging
 import datetime
 
-## Global Variables
-AUDIOFILE_DAY_CACHE_STORAGE = '/mnt/audiofile-day-cache/audio'
-## Set Defaults
-INTER_DELTA_SLEEP_TIME = 750
-DELTA_RETRIES = 2
-NO_PROGRESS_SLEEP_TIME = 120000
-CHUNK_SIZE = 4 * 1024 * 1024
+# Logging
 ADMIN_EMAILS = []  # While debugging the script
 #ADMIN_EMAILS = ['Ian.Llewellyn@rte.ie', 'radiomnt@rte.ie']
 STDERR_LOG_LEVEL = logging.INFO
 LOGFILE_LOG_LEVEL = logging.INFO
 EMAIL_LOG_LEVEL = logging.WARNING
-date = str(datetime.datetime.utcnow().date())
+
+# Files
+AUDIOFILE_DAY_CACHE_STORAGE = '/mnt/audiofile-day-cache/audio'
+LOCK_FILE = '/var/lock/subsys/af-sync.lock'
+
+# Other
+INTER_DELTA_SLEEP_TIME = 750
+DELTA_RETRIES = 2
+NO_PROGRESS_SLEEP_TIME = 120000
+CHUNK_SIZE = 520160
+DATE = str(datetime.datetime.utcnow().date())
+
+PORT_NUMBER = 11111
+CONFIG_PATH = '/etc/af-sync.d/af-sync.conf'
+
+LOG_FILE = '/var/log/audiofile//af-sync-controller.log'
+LOG_LEVEL = 2
+VERB_LEVEL = 1
